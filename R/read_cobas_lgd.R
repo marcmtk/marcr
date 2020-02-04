547e995544d8243261090ff1a7968eb99e07d0ba
Marc Trunjer Kusk Nielsen <marc.mtk@gmail.com>
2020-02-04 08:00:50 +0100

    Update read_cobas_lgd.R

--- R/read_cobas_lgd.R
+++ R/read_cobas_lgd.R
@@ -13,21 +13,22 @@
 #'
 #' curve_data <- read_cobas_lgd("data-raw/inf_b.lgd")
 read_cobas_lgd <- function(file) {
-  demarcations <- grepl("\\/\\/", readLines(file)) %>% which
+  demarcations <- grep("\\/\\/", readLines(file))
   len_dem <- length(demarcations)
-
+  
   targets <- read.table(file,
                         skip = demarcations[len_dem - 1],
                         header = FALSE,
                         nrows = demarcations[len_dem] - demarcations[len_dem - 1] - 2,
                         col.names = c("seq", "names", "colours"),
                         colClasses = "character")
-
+  
   read.table(file,
              skip = demarcations[len_dem],
              header = FALSE,
              col.names = c("cycle", targets$names),
              colClasses = "numeric") %>%
     tidyr::gather("channel", "flourescence", -cycle) %>%
-    dplyr::mutate(file = tools::file_path_sans_ext(basename(file)))
+    dplyr::mutate(folder = fs::path_dir(file),
+                  file = tools::file_path_sans_ext(basename(file)))
 }
